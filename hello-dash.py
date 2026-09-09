9aac9883de07a25592f4d92b97333747f04d9a67
luis-pseudo <luisangelpr31@gmail.com>
2026-09-09 21:24:08 +0000

    working on codespace

--- hello-dash.py
+++ hello-dash.py
@@ -1,6 +1,6 @@
 import streamlit as st
-import pandas as pd 
-st.title("hello streamlit")
+import pandas as pd
+st.title("hello world web")
+st.write("hello world streamlit")
 dataframe = pd.read_csv("https://raw.githubusercontent.com/luis-pseudo/data-science-5sem/master/Amazon_Reviews.csv", engine="python")
 st.dataframe(dataframe)
-st.write("by adsoftsito")
